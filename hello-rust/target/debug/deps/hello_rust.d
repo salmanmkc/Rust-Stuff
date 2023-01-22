c902b00c3d5da71a63fb799782b49846493b7038
SalmanMKC <13schishti@gmail.com>
2023-01-22 18:36:55 +0200

    rust project setup

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\rust-stuff\Rust-Stuff\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\rust-stuff\Rust-Stuff\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\rust-stuff\Rust-Stuff\hello-rust\target\debug\hello-rust.exe: C:\rust-stuff\Rust-Stuff\hello-rust\src\main.rs
